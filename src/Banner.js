259a8752b182e8120a67ca5eaa24de9e33d6ca1f
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-27 11:38:04 +0530

    Implement routing

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,12 @@
 import { Button } from '@material-ui/core'
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import './Banner.css'
 import Search from './Search'
 
 function Banner() {
     const [showSearch, setShowSearch] = useState(false)
+    const history = useHistory()
     
     return(
         <div className='banner'>
@@ -21,7 +23,8 @@ function Banner() {
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
                 <Button
-                    variant='outlined'>Explore Nearby</Button>
+                    variant='outlined'
+                    onClick={() => history.push('/search')}>Explore Nearby</Button>
             </div>
             
         </div>
